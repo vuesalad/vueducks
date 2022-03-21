6b09e9fe1f59f9df7cd66f629793540a0492502e
saladworks6725 <saladworks6725@gmail.com>
2022-03-20 23:43:22 -0400

    jsappbasic: remove because of useless

--- jsappbasic/bin/main.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-import * as util from '../lib/modulea.js';
-
-// Prints: 6
-console.log(util.addThree(4));
--- jsappbasic/lib/modulea.js
+++
@@ -1,9 +0,0 @@
-function addTwo(num) {
-    return num + 2;
-  }
-
-function addThree(num) {
-    return num + 3;
-  }
-  
-export { addTwo , addThree};
